5d48d75fcb1975df01c20775119262bb32daa022
RajuAhmed0 <itzraju98@gmail.com>
2025-04-09 11:38:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c9gyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    // Database & Collection
+    const allJobsColl = client.db("job_apply").collection("alljobs");
+
+
+
+    app.get("/allJobs", async (req, res) => {
+      const result = await allJobsColl.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/allJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await allJobsColl.findOne(query)
+      res.send(result)
+    })
+
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
