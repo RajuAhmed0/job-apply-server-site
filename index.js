5da5a0fa0f6607567bd2ecc5d620f4dc4375f501
RajuAhmed0 <itzraju98@gmail.com>
2025-04-17 15:13:51 +0600

    a

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c9gyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,10 +21,9 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
-    // Database & Collection
-    const allJobsColl = client.db("job_apply").collection("alljobs");
-
+    const db = client.db("job_apply");
+    const allJobsColl = db.collection("alljobs");
+    const applicationsColl = db.collection("applications");
 
 
     app.get("/allJobs", async (req, res) => {
@@ -35,12 +31,14 @@ async function run() {
       res.send(result);
     });
 
+
     app.get("/allJobs/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await allJobsColl.findOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await allJobsColl.findOne(query);
+      res.send(result);
+    });
+
 
     app.post("/allJobs", async (req, res) => {
       const data = req.body;
@@ -48,14 +46,45 @@ async function run() {
       res.send(result);
     });
 
+
+
+    app.get("/applications", async (req, res) => {
+      const result = await applicationsColl.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/applications", async (req, res) => {
+      try {
+        const application = {
+          ...req.body,
+          submitted_At: new Date(),
+          status: "pending"
+        };
+        const result = await applicationsColl.insertOne(application);
+        res.status(201).json({ message: "Application submitted", id: result.insertedId });
+      } catch (error) {
+        res.status(500).json({ message: "Error submitting application", error });
+      }
+    });
+
+    app.delete("/applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await applicationsColl.deleteOne(query);
+      res.send(result);
+    });
+
+
+
   } finally {
 
   }
 }
+
 run().catch(console.dir);
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
